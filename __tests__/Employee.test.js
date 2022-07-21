6f58a589a6e5a8c6fc6c6d63b31698a77d92e7ae
laurasierra17 <laura.sierra17@gmail.com>
2022-07-21 09:58:09 -0400

    passed second test for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,6 +2,7 @@ const { exportAllDeclaration } = require('@babel/types');
 const Employee = require('../lib/Employee');
 
 describe("Initialization", () => {
+    // Positive text
     it("should create an object with properties of name, id, and email when called with the 'new' keyword", () => {
         // Arrange
         const name = "Laura";
@@ -12,11 +13,27 @@ describe("Initialization", () => {
         const obj = new Employee(name, id, email);
 
         // Assert
-        expect(obj).toEqual({ name: name, id: id, email: email});
+        expect(obj).toEqual({ "name": name, "id": id, "email": email});
     })
+
+    // Exception test
+    // it("should throw an error if not provided any values", () => {
+    //     // Arrange
+    //     const employee = () => new Employee();
+    //     const err = new Error(
+    //         "Expect parameters 'name', 'id', 'email' to be non empty"
+    //     );
+
+    //     // Assert
+    //     expect(employee).toThrowError(err);
+    // })
 })
 
 describe("getName", () => {
+    it("should return the object's 'name' property value", () => {
+        const obj = new Employee("Laura", 4, "laura@laura.com");
+        expect(obj.getName()).toBe("Laura")
+    })
 
 })
 
