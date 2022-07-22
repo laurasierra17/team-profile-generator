f3f9f17c5828fade29c6c704a1803e1e25a6d474
laurasierra17 <laura.sierra17@gmail.com>
2022-07-22 01:05:38 -0400

    validate intern questionnaire

--- dist/internQuestions.js
+++ dist/internQuestions.js
@@ -2,22 +2,48 @@ const internQuestions = [
     {
         type: "input",
         name: "nameQ",
-        message: "What is your intern's name?"
+        message: "What is your intern's name?",
+        validate: (input) => {
+            if (!input) {
+                return "Please enter a valid name"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "idQ",
-        message: "What is your intern's id?"
+        message: "What is your intern's id?",
+        validate: (input) => {
+            if (!input) {
+                return "Please enter a valid id"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "emailQ",
-        message: "What is your intern's email?"
+        message: "What is your intern's email?",
+        // Validate user's email
+        validate: (input) => {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            if(!emailRegex.test(input)) {
+                return "You have to provide a valid email address"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "schoolQ",
-        message: "What is your intern's school?"
+        message: "What is your intern's school?",
+        validate: (input) => {
+            if (!input) {
+                return "Please enter a valid school"
+            }
+            return true;
+        }
     }
 ]
 
